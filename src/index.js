36dd6c8755453ee823111f05bccc682e60239c43
Ali Helmi <ahelmi365@gmail.com>
2024-09-24 15:09:19 +0300

    set html elms using js instead of duplicates html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const userCards = document.querySelectorAll(".user-card");
-  let users = [];
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((data) => (users = data))
-    // .then(() => console.log(users))
-    .then(() => users.forEach((user) => setUserData(user)));
+  const getUserData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        users.forEach((user) => {
+          setUserData(user);
+          setSideBarUserNames(user);
+        });
+      });
+  };
+  getUserData();
 
+  const userCardsContainer = document.getElementById("user-cards-container");
   const setUserData = (user) => {
-    const userCard = document.getElementById(user.id);
-    userCard.querySelector("#user-name").textContent = user.name;
-    userCard.querySelector("#user-phone").textContent = user.phone;
-    userCard.querySelector("#user-site").textContent = user.website;
-    userCard.querySelector("#user-email").textContent = user.email;
+    const userCardHTML = `
+      <div class="user-card" id=${user.id}>
+        <h3 class="card-title" id="user-name">${user.name}</h3>
+        <div id="card-body">
+          <p id="user-phone">${user.phone}</p>
+          <p id="user-site">${user.website}</p>
+          <p id="user-email">${user.email}</p>
+          <a href="./user-posts/index.html?id=${user.id}&name=${user.name}" id="userDetailsLink">
+            Check Posts...
+          </a>
+        </div>
+      </div>
+    `;
+    userCardsContainer.innerHTML += userCardHTML;
+  };
+
+  const sideBarUserNamesElm = document.getElementById("side-bar-user-list");
+  const setSideBarUserNames = (user) => {
+    const newLiElm = `
+    <li>
+      <a href="./user-posts/index.html?id=${user.id}&name=${user.name}">${user.name}</a>
+    </li>
+    `;
+    sideBarUserNamesElm.innerHTML += newLiElm;
   };
 });
